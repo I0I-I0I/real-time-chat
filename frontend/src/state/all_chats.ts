bd2d342dcd27e8a3668792d53e0f3d82ece3e758
I0I <azudjndj@gmail.com>
2025-04-09 00:12:39 +0300

    add: chats list store

---
+++ frontend/src/state/all_chats.ts
@@ -0,0 +1,27 @@
+import { IChat, IMessage } from "@/types"
+import { create } from "zustand"
+
+interface ChatsListStore {
+    data: IChat[],
+    lastMessage: IMessage | null,
+    setChatsList: (chat: IChat[]) => void
+    setLastMessage: (chat: IChat, message: IMessage) => void
+    getLastMessage: (chat: IChat) => string | null
+}
+
+export const useChatsListStore = create<ChatsListStore>()((set, get) => ({
+    data: [],
+    lastMessage: null,
+    setChatsList: (chats: IChat[]) => set({ data: chats }),
+    setLastMessage: (chat: IChat, message: IMessage) => {
+        const { data } = get()
+        const current_chat = data.find(c => c.id === chat.id)
+        chat.lastMessage = message
+        set({ data: { ...data, ...current_chat} })
+    },
+    getLastMessage: (chat: IChat) => {
+        const { data } = get()
+        const lastMessage = data.find(c => c.id === chat.id)?.lastMessage?.body
+        return lastMessage ? lastMessage : null
+    }
+}))
